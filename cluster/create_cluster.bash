3fdceebb35fce031ff24de05d167e3145b1fc5e1
fukuta-tatsuya-intec <fukuta_tatsuya@intec.co.jp>
2022-02-04 08:14:59 +0900

    Successfully tested Grafana (except for the Live function).

--- cluster/create_cluster.bash
+++ cluster/create_cluster.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 kind create cluster --config kind_cluster.yaml
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
+echo ""
+echo "---"
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl wait --timeout=180s -n kube-system --for=condition=available daemonSet/weave-net
+echo "weave-net has been installed. Check its status by running:"
+echo "  kubectl get node -o wide"
