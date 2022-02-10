634ab1098e4a2c9ec61b77161b3cb31b747a7008
fukuta-tatsuya-intec <fukuta_tatsuya@intec.co.jp>
2022-02-10 16:53:22 +0900

    Confirmed that the status of the GitLab component group is displayed in all green.

--- cluster/create_cluster.bash
+++ cluster/create_cluster.bash
@@ -11,7 +11,9 @@ kind create cluster --config kind_cluster.yaml
 # Step2 Weave-Net Install
 echo ""
 echo "---"
+echo "Installing Weave-Net ..."
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 kubectl wait --timeout=180s -n kube-system --for=condition=ready pod -l name=weave-net
+echo "---"
 echo "weave-net has been installed. Check its status by running:"
 echo "  kubectl get node -o wide"
