3dcc2c9d7648427c9abd6d678ca1fabfb11da243
fukuta-tatsuya-intec <fukuta_tatsuya@intec.co.jp>
2022-02-19 17:18:05 +0900

    Automate processes that depend on IP addresses

--- cluster/create_cluster.bash
+++ cluster/create_cluster.bash
@@ -6,8 +6,13 @@ set -euo pipefail
 #  to run a ROS2 app on a Kubernetes cluster.
 ###################
 
+if [ $# != 1 ]; then
+  echo "ERROR:Specify the cluster name."
+  exit 1
+fi
+
 # Step1 KinD Install
-kind create cluster --config kind_cluster.yaml
+kind create cluster --config values_for_kind-cluster.yaml --name "$1"
 
 # Step2 Weave-Net Install
 echo ""
@@ -16,5 +21,8 @@ echo "Installing Weave-Net ..."
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 kubectl wait --timeout=180s -n kube-system --for=condition=ready pod -l name=weave-net
 echo ""
-echo "Weave-Net has been installed. Check its status by running:"
+echo "---"
+echo "KinD-Cluster and Weave-Net has been installed. Check its status by running:"
 echo "  kubectl get node -o wide"
+
+exit 0
--- ingress/create_ingress.bash
+++ ingress/create_ingress.bash
@@ -1,14 +1,25 @@
 #!/bin/bash
 set -euo pipefail
 
+## ./create_ingress.bash rdbox nip.io
+
+sub_domain=$1
+dns_service=$2
 flag="new-rootca" # or recycle(For Developpers)
-if [ $# -le 1 ]; then
-  flag=$1
+if [ $# -eq 3 ]; then
+  flag=$3
 fi
 echo "Mode: $flag"
 
-HISTORY_FILE=rdbox-selfsigned-ca.rdbox.172-16-0-110.nip.io.ca.yaml
-ROOTCA_FILE=rdbox.172-16-0-110.nip.io.ca.crt
+DEFULT_NIC=$(netstat -rn | grep default | grep -v ":" | awk '{print $4}')
+# shellcheck disable=SC2015
+IP_DEFAULT_NIC=$( (command -v ip &> /dev/null && ip addr show "$DEFULT_NIC" || ifconfig "$DEFULT_NIC") | \
+                  sed -nEe 's/^[[:space:]]+inet[^[:alnum:]]+([0-9.]+).*$/\1/p')
+#HOSTNAME_NIPIO_BASED_ON_IP=$(echo "$IP_DEFAULT_NIC" | awk -F. '{printf "%02x", $1}{printf "%02x", $2}{printf "%02x", $3}{printf "%02x", $4}')
+HOSTNAME_NIPIO_BASED_ON_IP=${IP_DEFAULT_NIC//\./-}
+FQDN_THIS_CLUSTER="$sub_domain"."$HOSTNAME_NIPIO_BASED_ON_IP"."$dns_service"
+HISTORY_FILE=rdbox-selfsigned-ca.${FQDN_THIS_CLUSTER}.ca.yaml
+ROOTCA_FILE=${FQDN_THIS_CLUSTER}.ca.crt
 
 # Step1 cert-manager Install
 echo ""
@@ -28,11 +39,12 @@ echo "---"
 echo "Setup RootCA and Specific Issuer ..."
 if [ "$flag" = "new-rootca" ]; then
   kubectl apply -f values_for_cert-manager-rootca.yaml
-  kubectl -n cert-manager get secrets rdbox-selfsigned-ca-cert -o yaml > $HISTORY_FILE
-  kubectl -n cert-manager get secrets rdbox-selfsigned-ca-cert -o json | jq -r '.data["ca.crt"]' | base64 -d > $ROOTCA_FILE
+  while ! kubectl -n cert-manager get secret rdbox-selfsigned-ca-cert; do sleep 2; done
+  kubectl -n cert-manager get secrets rdbox-selfsigned-ca-cert -o yaml > "$HISTORY_FILE"
+  kubectl -n cert-manager get secrets rdbox-selfsigned-ca-cert -o json | jq -r '.data["ca.crt"]' | base64 -d > "$ROOTCA_FILE"
 elif [ "$flag" = "recycle" ]; then
   if [ -e "$HISTORY_FILE" ]; then
-    kubectl -n cert-manager apply -f $HISTORY_FILE
+    kubectl -n cert-manager apply -f "$HISTORY_FILE"
   else
     echo "No history file found. Please generate a new RootCA."
     exit 1
@@ -65,21 +77,61 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/metallb.yaml
 kubectl wait --timeout=180s -n metallb-system --for=condition=available deployment/controller
+
+# Step5 Config MetalLB with L2 Mode
 #docker network inspect kind | jq -r ".[].IPAM.Config[].Subnet" | grep -v ":"
-# Create multiple YAML objects from stdin
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  namespace: metallb-system
-  name: config
-data:
-  config: |
-    address-pools:
-    - name: default
-      protocol: layer2
-      addresses:
-      - 172.18.255.200-172.18.255.250
+NETWORK_IP=$(docker network inspect kind | jq -r ".[].IPAM.Config[].Subnet" | grep -v ":" | awk -F/ '{print $1}')
+NETWORK_PREFIX=$(docker network inspect kind | jq -r ".[].IPAM.Config[].Subnet" | grep -v ":" | awk -F/ '{print $2}')
+if [ "$NETWORK_PREFIX" -le 16 ]; then
+  NETWORK_RANGE=$(echo "$NETWORK_IP" | awk -F. '{printf "%s.%s.%s-%s.%s.%s", $1, $2, "255.200", $1, $2, "255.250"}')
+elif [ "$NETWORK_PREFIX" -gt 16 ] && [ "$NETWORK_PREFIX" -le 24 ]; then
+  NETWORK_RANGE=$(echo "$NETWORK_IP" | awk -F. '{printf "%s.%s.%s.%s-%s.%s.%s.%s", $1, $2, $3, "200", $1, $2, $3, "250"}')
+else
+  echo ""
+  echo "---"
+  echo "WARN: Your Docker network configuration is not expected;"
+  echo "  You will need to execute the MetalLB configuration yourself."
+  echo "  https://kind.sigs.k8s.io/docs/user/loadbalancer/#setup-address-pool-used-by-loadbalancers"
+  exit 1
+fi
+
+echo ""
+echo "---"
+echo "MetalLB will reserve the following IP address ranges."
+echo "  $NETWORK_RANGE"
+echo ""
+read -r -n1 -p "ok? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+  # Create multiple YAML objects from stdin
+  echo ""
+  cat <<EOF | kubectl apply -f -
+  apiVersion: v1
+  kind: ConfigMap
+  metadata:
+    namespace: metallb-system
+    name: config
+  data:
+    config: |
+      address-pools:
+      - name: default
+        protocol: layer2
+        addresses:
+        - $NETWORK_RANGE
 EOF
+  echo ""
+  kubectl -n metallb-system describe configmaps config
+else
+  echo "abort"
+  echo "  You will need to execute the MetalLB configuration yourself."
+  echo "  https://kind.sigs.k8s.io/docs/user/loadbalancer/#setup-address-pool-used-by-loadbalancers"
+  exit 1
+fi
+
+echo ""
+echo "---"
+echo "The basic network modules has been installed. Check its status by running:"
+echo "  kubectl -n cert-manager get pod"
+echo "  kubectl -n ambassador get pod"
+echo "  kubectl -n metallb-system get pod"
 
 exit 0
