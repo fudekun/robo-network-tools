ae71acec2a5f8247f73a3b98c6b7a07becc45bf4
fukuta-tatsuya-intec <fukuta_tatsuya@intec.co.jp>
2022-07-22 16:36:48 +0900

    #69 new branch

--- modules/modules/k8s-dashboard/k8s-dashboard.bash
+++ modules/modules/k8s-dashboard/k8s-dashboard.bash
@@ -4,7 +4,7 @@ set -euo pipefail
 function showHeaderCommand() {
   echo ""
   echo "---"
-  echo "## Installing ${PACKAGE_NAME} ..."
+  echo "## Installing k8s-dashboard ..."
   return $?
 }
 
@@ -31,10 +31,27 @@ function showVerifierCommand() {
 }
 
 function __executor() {
+  local __SPECIFIC_SECRETS="specific-secrets"
+  local __namespace_for_dashboard
+  ## 1. Config extra secrets
+  ##
+  echo ""
+  echo "### Setting Config of k8s-dashboard ..."
+  __namespace_for_dashboard="$(getNamespaceName "k8s-dashboard")"
+  if ! kubectl create namespace "${__namespace_for_dashboard}" 2>/dev/null; then
+    echo "already exist the namespace (${__namespace_for_dashboard}) ...ok"
+  fi
+  if kubectl -n "${__namespace_for_dashboard}" get secret "${__SPECIFIC_SECRETS}" 2>/dev/null; then
+    echo "already exist the secrets (${__SPECIFIC_SECRETS}.${__namespace_for_dashboard}) ...ok"
+  else
+    kubectl -n "${__namespace_for_dashboard}" create secret generic "${__SPECIFIC_SECRETS}" \
+      --from-literal=k8s-default-cluster-sso-aes-secret="$(openssl rand -base64 32 | sed -e 's/\+/\@/g')"
+  fi
+  ## 2. Setup a dummy endpoint of kube-apiserver for the k8s-dashboard
+  ##
   return $?
 }
 
 source "${RDBOX_WORKDIR_OF_SCRIPTS_BASE}/modules/libs/common.bash"
 main "$@"
 exit $?
-
